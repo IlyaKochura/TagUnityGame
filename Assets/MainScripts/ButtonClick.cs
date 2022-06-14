44791d1f022a95783c811383e9d144647e557321
IlyaKochura <ikochura@inbox.ru>
2022-06-14 19:50:35 +0300

    Попытка сделать перемешивание

--- Assets/MainScripts/ButtonClick.cs
+++ Assets/MainScripts/ButtonClick.cs
@@ -7,21 +7,26 @@ namespace MainScripts
 {
     public class ButtonClick : MonoBehaviour
     {
+        public Image _img;
         public TextMeshProUGUI text;
         public Action<int> Delegate { get; set; }
         private Button _button;
         public int id { get; set; }
-        
+
         void Start()
         {
             _button = GetComponent<Button>();
-            _button.onClick.AddListener( () => Delegate.Invoke(id));
+            _button.onClick.AddListener(() => Delegate.Invoke(id));
         }
-        
+
+        public void SetColor()
+        {
+            _img.color = Color.green;
+        }
+
         public void ChangeText(string newText)
         {
             text.text = newText;
         }
-    
     }
 }
--- Assets/MainScripts/GameController.cs
+++ Assets/MainScripts/GameController.cs
@@ -14,42 +14,45 @@ public class GameController : MonoBehaviour
     [SerializeField] private MixButton mixingButton;
     [SerializeField] private List<ButtonClick> buttonList;
 
-  
+
     private int _currentId;
 
     void Start()
     {
         _currentId = fieldSize * fieldSize - 1;
         buttonList = new List<ButtonClick>(fieldSize * fieldSize);
-        
-        
+
+
         grid.constraintCount = fieldSize;
         for (int i = 0; i < fieldSize * fieldSize; i++)
         {
             var slot = Instantiate(prefabButon, canvasPosition);
+
+
             buttonList.Add(slot);
-            buttonList[i].ChangeText((i+1).ToString());
-            
+            buttonList[i].ChangeText((i + 1).ToString());
+
             if (i == fieldSize * fieldSize - 1)
             {
                 buttonList[i].ChangeText("");
             }
-            
         }
-
+        
+        mixingButton.action = MixingCells;
+        
         for (int i = 0; i < fieldSize * fieldSize; i++)
         {
             var id = i;
             buttonList[i].id = i;
             buttonList[i].Delegate = MoveButton;
         }
-
-        
     }
-    
+
     private void MoveButton(int index)
     {
-        if ((index + 1 == _currentId  && (_currentId % fieldSize) != 0) ||
+        Debug.LogError(index);
+
+        if ((index + 1 == _currentId && (_currentId % fieldSize) != 0) ||
             (index - 1 == _currentId && (_currentId + 1) % fieldSize != 0) ||
             index + fieldSize == _currentId ||
             index - fieldSize == _currentId)
@@ -63,11 +66,9 @@ public class GameController : MonoBehaviour
             var b2 = GetButtonById(_currentId);
             b1.id = _currentId;
             b2.id = index;
-            
+
             _currentId = index;
         }
-
-        MixingCells();
     }
 
     private ButtonClick GetButtonById(int id)
@@ -79,34 +80,30 @@ public class GameController : MonoBehaviour
         }
 
         return null;
-        
     }
 
     private void MixingCells()
     {
         Random rnd = new Random();
 
+        Debug.LogError(buttonList.Count);
+
         for (int i = 0; i < fieldSize * fieldSize; i++)
         {
             var RND = rnd.Next(0, fieldSize * fieldSize - 1);
             var posI = buttonList[i].transform.localPosition;
             var posRND = buttonList[RND].transform.localPosition;
-            buttonList[i].transform.localPosition = new Vector3(posRND.x,posRND.y);
-            buttonList[RND].transform.localPosition = new Vector3(posI.x,posI.y);
-            
+
+            Debug.LogError(buttonList[i].transform.localPosition);
+            Debug.LogError(buttonList[RND].transform.localPosition);
+
+
+            buttonList[i].transform.localPosition = new Vector3(posRND.x, posRND.y);
+            buttonList[RND].transform.localPosition = new Vector3(posI.x, posI.y);
+
+            Debug.LogError(buttonList[i].transform.localPosition);
+            Debug.LogError(buttonList[RND].transform.localPosition);
         }
+        
     }
 }
-
-// public int ItIsMark()
-// {
-//     for (int i = 0; i < buttonList.Count; i++)
-//     {
-//         if (buttonList[i].itIsImage)
-//         {
-//             return i;
-//             break;
-//         }
-//     }
-//     return 0;
-// }
